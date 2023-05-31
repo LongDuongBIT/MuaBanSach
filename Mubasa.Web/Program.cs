86be7b11e3ffc2edc964ca88eb68113b9a3b5004
Long Duong Huu <long.duonghuu@nashtechglobal.com>
2023-05-31 23:06:31 +0700

    For deploy

---
+++ Mubasa.Web/Program.cs
@@ -0,0 +1,58 @@
+using Mubasa.DataAccess.DbInitializer;
+using Mubasa.Web.Installers;
+
+namespace Mubasa.Web
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.InstallServicesInAssembly(builder.Configuration);
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            SeedDatabase();
+
+            app.UseRequestLocalization();
+
+            // app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+
+            app.UseSession();
+
+            app.MapRazorPages();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+
+            void SeedDatabase()
+            {
+                using (var scope = app.Services.CreateScope())
+                {
+                    var dbInit = scope.ServiceProvider.GetRequiredService<IDbInitializer>();
+                    dbInit.Initialize();
+                }
+            }
+        }
+    }
+}
